a587a6c92bbf02262d7e602402f797d7809aa323
AntonioIIOlaguer <antonio.olaguer.ii@gmail.com>
2022-08-13 13:29:18 -0400

    Fixed Get_destination_code to get the right Code

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,17 @@
+import os
+import requests
+from datetime import datetime
+HEADER = {"apikey" : os.environ.get("TEQUILA_KEY")}
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def get_destination_code(self,city):
+        self.query = {
+            "term":city,
+            "location_types":"city"
+        }
+        self.response = requests.get(url=f"{TEQUILA_ENDPOINT}/locations/query",params=self.query, headers=HEADER)
+        self.response.raise_for_status()
+        self.code = self.response.json()["locations"][0]["code"]
+        return self.code
