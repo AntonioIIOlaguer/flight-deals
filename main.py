6df40233049366f9d3aa440db8cabb094091500a
AntonioIIOlaguer <antonio.olaguer.ii@gmail.com>
2022-08-18 13:32:45 -0400

    Initial files to get the iata codes and fill it to the sheet

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+
+for destination in data_manager.destinations:
+    destination["iataCode"] = flight_search.get_destination_code(destination["city"])
+
+data_manager.fill_iata(data_manager.destinations)
+
+print(data_manager.destinations)
